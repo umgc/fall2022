be98ee458cb04667e6410b405e8869db5f1b0537
Shane K <shanelknowles@gmail.com>
2022-09-26 15:54:22 -0400

    Adding tests

--- test/services/chatbot_service_test.dart
+++ test/services/chatbot_service_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:summer2022/services/chat_bot_service.dart';
 import 'package:summer2022/utility/RouteGenerator.dart';
+import 'package:summer2022/models/SearchCriteria.dart';
 
 void main() async {
   TestWidgetsFlutterBinding.ensureInitialized();
@@ -18,6 +19,26 @@ void main() async {
       var result = service.performChatFunction(SiteAreas.Settings, "home");
       expect(result.methodName, "navigateTo");
       expect(result.parameters, <String>["/main"]);
+    });
+    test("Verify Search with Parameters multiple keywords", () {
+      var service = ChatBotService();
+      var result = service.performChatFunction(SiteAreas.Search, "search 10/2/2022 keyword 10/7/2022 test");
+      var searchCriteria = SearchCriteria.withList(result.parameters!);
+      expect(searchCriteria.keyword, "keyword test");
+    });
+    test("Verify Search with Parameters Start and End Dates", () {
+      var service = ChatBotService();
+      var result = service.performChatFunction(SiteAreas.Search, "search 10/2/2022 10/7/2022");
+      var searchCriteria = SearchCriteria.withList(result.parameters!);
+      expect(searchCriteria.startDate, DateTime(2022, 10, 2));
+      expect(searchCriteria.endDate, DateTime(2022, 10, 7));
+    });
+    test("Verify Search with Parameters Start Exceeds End Dates", () {
+      var service = ChatBotService();
+      var result = service.performChatFunction(SiteAreas.Search, "search 10/20/2022 10/7/2022");
+      var searchCriteria = SearchCriteria.withList(result.parameters!);
+      expect(searchCriteria.startDate, DateTime(2022, 10, 20));
+      expect(searchCriteria.endDate, DateTime(2022, 10, 20));
     });
   });
 }
