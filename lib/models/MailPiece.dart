93a3d53d84193524d37d18afa224e85ee30566a1
sHuber <111936253+shiftClick@users.noreply.github.com>
2022-09-26 22:42:13 -0400

    Create MailPiece.dart
    sample commit, test

---
+++ lib/models/MailPiece.dart
@@ -0,0 +1,10 @@
+class MailPiece {
+  int _mail_ID;
+  DateTime _informedDate;
+  String _LogoText;
+  AddressObject _senderAddress;
+
+   MailPiece( this._mail_ID, this._informedDate, this._LogoText, this._senderAddress );
+
+  
+}
