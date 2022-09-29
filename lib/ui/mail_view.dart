2603ae22477bc90fbbb76c32d92324d314132ee1
tatikozh <35759841+tatikozh@users.noreply.github.com>
2022-09-29 18:33:47 +0200

    Feature/449 (#497)
    * Added semantics to search.dart, mail_view.dart
    
    * Added semantics and logic to not display elements in the top bar on the login and main menu screens
    
    * merged with dev
    
    Co-authored-by: mdconatser <mdconatser@gmail.com>

--- lib/ui/mail_view.dart
+++ lib/ui/mail_view.dart
@@ -4,11 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:summer2022/models/MailPiece.dart';
 import 'package:summer2022/ui/bottom_app_bar.dart';
+import 'package:summer2022/ui/top_app_bar.dart';
 
 class MailViewWidget extends StatelessWidget {
-  final FontWeight _commonFontWeight = FontWeight.w500;
-  final double _commonFontSize = 30;
-  final Color _buttonColor = Color.fromRGBO(51, 51, 102, 1.0);
   final List<MailPiece> mailPieces = List.generate(
       10,
           (index) =>
@@ -16,64 +14,73 @@ class MailViewWidget extends StatelessWidget {
   );
   @override
   Widget build(BuildContext context) {
-    Widget _buildMailPiece(MailPiece mailPiece) {
+    Widget _buildMailPiece(BuildContext context, MailPiece mailPiece) {
       return Container(
         color: Colors.white10,
-        child: ListTile(
-          horizontalTitleGap: 10.0,
-          contentPadding: EdgeInsets.all(5),
-          dense: true,
-          onTap: () {
-            Navigator.pushNamed(context, '/mail_piece_view', arguments: mailPiece);
-          },
-          //leading: mailPiece.mailImage,
-          title:
-          Row(
-              children:[
-                Container(
-                  padding: EdgeInsets.only(right: 10.0),
-                  child:
-                  Text(
-                    mailPiece.sender,
-                    style: TextStyle(fontSize: 20),
-                  ),
-                ),
-                Expanded(
-                  child:
-                  Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children:[
-                        Text(DateFormat('EEE hh:mm').format(mailPiece.timestamp)
-                        ),
-                        Text(DateFormat('MM/dd/yyyy').format(mailPiece.timestamp)
-                        ),
-                      ]),
-                ),
-              ]
+        child:
+        GestureDetector(
+          child:
+          Semantics(
+            excludeSemantics: true,
+            button: true,
+            label: "Letter from ${mailPiece.sender} received on ${DateFormat('EEE MMM,d,yyyy').format(mailPiece.timestamp)}",
+            hint: "Double tap to select.",
+            child:
+            ListTile(
+              horizontalTitleGap: 10.0,
+              contentPadding: EdgeInsets.all(5),
+              dense: true,
+              onTap: () {
+                Navigator.pushNamed(context, '/mail_piece_view', arguments: mailPiece);
+              },
+              //leading: mailPiece.mailImage,
+              title:
+              Row(
+                  children:[
+                    Container(
+                      padding: EdgeInsets.only(right: 10.0),
+                      child:
+                      Text(
+                        mailPiece.sender,
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ),
+                    Expanded(
+                      child:
+                      Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children:[
+                            Text(DateFormat('EEE hh:mm').format(mailPiece.timestamp)
+                            ),
+                            Text(DateFormat('MM/dd/yyyy').format(mailPiece.timestamp)
+                            ),
+                          ]),
+                    ),
+                  ]
+              ),
+              subtitle: Text(mailPiece.imageText.toString()),
+            ),
           ),
-          subtitle: Text(mailPiece.imageText.toString()),
         ),
       );
-    };
+    }
 
     return Scaffold(
       bottomNavigationBar: const BottomBar(),
-      appBar: AppBar(
-        title: Text('Search Results',  style:
-        TextStyle(fontWeight: _commonFontWeight, fontSize: _commonFontSize),
-        ),
-        backgroundColor: _buttonColor,
-        centerTitle: true,
+      appBar: TopBar(
+          title: "Search Results"
       ),
       body: Container(
         padding: EdgeInsets.all(15.0),
         child: Center(
-          child: ListView(
-            children: mailPieces.map(_buildMailPiece).toList(),
+          child: ListView.builder(
+              itemCount: mailPieces.length,
+              itemBuilder: (context, int index) {
+                return _buildMailPiece(context, mailPieces[index]);
+              }
           ),
         ),
       ),
     );
   }
 }
-
