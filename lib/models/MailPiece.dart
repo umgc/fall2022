20c7f8ab186e05932ca9a8bb41f0c3a3429fa2a0
umgcjack <111916720+umgcjack@users.noreply.github.com>
2022-09-29 06:02:35 -0700

    add sqlite (#496)
    Co-authored-by: atgJack <jack@atg.io>
    Co-authored-by: Shane Knowles <shanelknowles@gmail.com>

--- lib/models/MailPiece.dart
+++ lib/models/MailPiece.dart
@@ -1,11 +1,8 @@
-import 'dart:core';
-import 'package:enough_mail/enough_mail.dart';
-import 'package:intl/intl.dart';
-import 'package:summer2022/models/Digest.dart';
-
+/// This class represents a specific piece of mail recieved by the USPS.
+/// There will likely be multiple of these items per email received.
 class MailPiece {
-  String? id; // Unique ID of Image
-  final String mailId; // Unique ID of Email - made from Email contents hashed
+  final String id; // Unique ID of Image
+  final String emailId; // Unique ID of Email - made from Email contents hashed
   final DateTime timestamp;
   final String sender;
   final String imageText;
