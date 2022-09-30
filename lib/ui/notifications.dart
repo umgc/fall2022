4d36a50b31e8c6a3276f073bd12fb5ee92e9bf3d
hgadani <hgadani@student.umgc.edu>
2022-09-30 12:35:49 -0400

    Made more changes to the notifications view.

--- lib/ui/notifications.dart
+++ lib/ui/notifications.dart
@@ -30,27 +30,27 @@ class NotificationsWidgetState extends State<NotificationsWidget> {
       child: Scaffold(
         bottomNavigationBar: const BottomBar(),
         appBar: AppBar(
-            actions: <Widget>[
-              IconButton(
-                  icon: new Image.asset("assets/icon/settings-icon.png", width: 30, height: 30), onPressed: () {Navigator.pushNamed(context, '/settings');} ),
-              IconButton(
-                  icon: new Image.asset("assets/icon/exit-icon.png", width: 30, height: 30), onPressed: () {Navigator.pushNamed(context, '/sign_in');} ),
-            ],
-            leading:
+          actions: <Widget>[
             IconButton(
-              icon: new Image.asset("assets/icon/back-icon.png", width: 30, height: 30), onPressed: () { navKey.currentState!.pushNamed('/main');}, ),
-            centerTitle: true,
+                icon: new Image.asset("assets/icon/settings-icon.png", width: 30, height: 30), onPressed: () {Navigator.pushNamed(context, '/settings');} ),
+            IconButton(
+                icon: new Image.asset("assets/icon/exit-icon.png", width: 30, height: 30), onPressed: () {Navigator.pushNamed(context, '/sign_in');} ),
+          ],
+          leading:
+          IconButton(
+            icon: new Image.asset("assets/icon/back-icon.png", width: 30, height: 30), onPressed: () { navKey.currentState!.pushNamed('/main');}, ),
+          centerTitle: true,
 
-            title: Text("Notifications",
-              style:
-              TextStyle(fontWeight: FontWeight.w700, fontSize: 30),
-            ),
-            automaticallyImplyLeading: false,
-            backgroundColor: Color.fromRGBO(51, 51, 102, 1),
-            bottom: const TabBar(
+          title: Text("Notifications",
+            style:
+            TextStyle(fontWeight: FontWeight.w700, fontSize: 30),
+          ),
+          automaticallyImplyLeading: false,
+          backgroundColor: Color.fromRGBO(51, 51, 102, 1),
+          bottom: const TabBar(
               tabs: <Widget>[
                 Tab(text: "Notifications",), Tab(text: "Manage",)
-            ]
+              ]
           ),
         ),
         body: const TabBarView(
@@ -65,5 +65,5 @@ class NotificationsWidgetState extends State<NotificationsWidget> {
         ),
       ),
     );
-    }
+  }
 }
