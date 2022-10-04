259a35272b5a2d9a90ea5a2b2fd4ad2ee006c81e
Minyahil K <minyahil@Borkena-Pro.local>
2022-10-04 00:50:33 -0500

    clean up impl

--- lib/ui/sign_in.dart
+++ lib/ui/sign_in.dart
@@ -385,71 +385,6 @@ class SignInWidgetState extends State<SignInWidget> {
     );
   }
 
-
-  // sign in with apple
-  /// Generates a cryptographically secure random nonce, to be included in a
-/// credential request.
-String generateNonce([int length = 32]) {
-  final charset =
-      '0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._';
-  final random = Random.secure();
-  return List.generate(length, (_) => charset[random.nextInt(charset.length)])
-      .join();
-}
-
-/// Returns the sha256 hash of [input] in hex notation.
-String sha256ofString(String input) {
-  final bytes = utf8.encode(input);
-  final digest = sha256.convert(bytes);
-  return digest.toString();
-}
-
-Future<UserCredential> signInWithApple() async {
-  // To prevent replay attacks with the credential returned from Apple, we
-  // include a nonce in the credential request. When signing in with
-  // Firebase, the nonce in the id token returned by Apple, is expected to
-  // match the sha256 hash of `rawNonce`.
-  final rawNonce = generateNonce();
-  final nonce = sha256ofString(rawNonce);
-
-  // Request credential for the currently signed in Apple account.
-  final appleCredential = await SignInWithApple.getAppleIDCredential(
-    scopes: [
-      AppleIDAuthorizationScopes.email,
-      AppleIDAuthorizationScopes.fullName,
-    ],
-    nonce: nonce,
-  );
-
-  // Create an `OAuthCredential` from the credential returned by Apple.
-  final oauthCredential = OAuthProvider("apple.com").credential(
-    idToken: appleCredential.identityToken,
-    rawNonce: rawNonce,
-  );
-
-  // Sign in the user with Firebase. If the nonce we generated earlier does
-  // not match the nonce in `appleCredential.identityToken`, sign in will fail.
-  return await FirebaseAuth.instance.signInWithCredential(oauthCredential);
-}
-
-/// sign in with google
-Future<UserCredential> signInWithGoogle() async {
-  // Trigger the authentication flow
-  final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
-
-  // Obtain the auth details from the request
-  final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
-
-  // Create a new credential
-  final credential = GoogleAuthProvider.credential(
-    accessToken: googleAuth?.accessToken,
-    idToken: googleAuth?.idToken,
-  );
-
-  // Once signed in, return the UserCredential
-  return await FirebaseAuth.instance.signInWithCredential(credential);
-}
-
   Future<void> _signInWithGoogle() async {
     // Trigger the authentication flow
     final googleUser = await GoogleSignIn().signIn();
@@ -491,26 +426,6 @@ Future<UserCredential> signInWithGoogle() async {
       await _auth.signInWithProvider(yahooProvider);
     }
   }
-
-
-
-// sign in with github
-Future<UserCredential> signInWithGitHub() async {
-  // Create a GitHubSignIn instance
-      final GitHubSignIn gitHubSignIn = GitHubSignIn(
-          clientId: 'clientId',
-          clientSecret: 'clientSecret',
-          redirectUrl: 'https://my-project.firebaseapp.com/__/auth/handler');
-
-  // Trigger the sign-in flow
-  final result = await gitHubSignIn.signIn(context);
-
-  // Create a credential from the access token
-  final githubAuthCredential = GithubAuthProvider.credential('result.token');
-
-  // Once signed in, return the UserCredential
-  return await FirebaseAuth.instance.signInWithCredential(githubAuthCredential);
-}
 
   Future<void> _signInWithGitHub() async {
     final githubProvider = GithubAuthProvider();
