009ef80857d9371eccc5bac41cba3ebd173eadc8
cthorn2 <111912838+cthorn2@users.noreply.github.com>
2022-10-09 13:41:58 -0400

    Update top_app_bar.dart

--- lib/ui/top_app_bar.dart
+++ lib/ui/top_app_bar.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 import '../main.dart';
 
 class TopBar extends StatefulWidget implements PreferredSizeWidget {
