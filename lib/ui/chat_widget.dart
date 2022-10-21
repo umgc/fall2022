b1bd426c142c047c1d0c1013edce9d1550a7a49a
Shane K <shanelknowles@gmail.com>
2022-10-21 10:30:11 -0400

    Refactoring Chatbot and adding Notification, upload, scan mail options

--- lib/ui/chat_widget.dart
+++ lib/ui/chat_widget.dart
@@ -7,9 +7,12 @@ import 'package:flutter/services.dart' show rootBundle;
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 import 'package:flutter_chat_ui/flutter_chat_ui.dart';
 import 'package:http/http.dart' as http;
+import 'package:image_picker/image_picker.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:summer2022/models/ApplicationFunction.dart';
+import 'package:summer2022/models/NotificationSubscription.dart';
 import 'package:summer2022/services/chat_bot_service.dart';
+import 'package:summer2022/services/mail_notifier.dart';
 import 'package:summer2022/ui/floating_home_button.dart';
 import 'package:summer2022/utility/RouteGenerator.dart';
 import 'package:summer2022/ui/top_app_bar.dart';
@@ -18,6 +21,8 @@ import 'package:uuid/uuid.dart';
 
 import 'package:summer2022/services/analytics_service.dart';
 import 'package:summer2022/utility/locator.dart';
+
+import '../services/mail_loader.dart';
 
 class ChatWidget extends StatefulWidget {
   final SiteAreas currentPage;
@@ -33,6 +38,7 @@ class _ChatWidgetState extends State<ChatWidget> {
   final _system = const types.User(id: 'system', /* imageUrl: TODO: Add chatBot image */);
   final FontWeight _commonFontWt = FontWeight.w700;
   final double _commonFontSize = 30;
+  final mailLoader = MailLoader();
   List<types.Message> _messages = [];
 
   @override
@@ -96,15 +102,35 @@ class _ChatWidgetState extends State<ChatWidget> {
 
     // Get functionality from bot
     ApplicationFunction chatFunction = _chatBotService.performChatFunction(widget.currentPage, message.text.toLowerCase());
-    if (chatFunction.message.isNotEmpty) _addSystemMessage(chatFunction.message);
+    chatFunction.messages?.forEach((element) {
+      _addSystemMessage(element);
+    });
 
-    // TODO: Perform functions
+    // Perform functions
     switch (chatFunction.methodName) {
+      case 'addNotification':
+        var subscription = new NotificationSubscription(chatFunction.parameters!.join(','));
+        final _notifier = MailNotifier();
+        _notifier.createSubscription(subscription);
+        _addSystemMessage("Notification for ${subscription.keyword} has been added.");
+        break;
+      case 'deleteNotification':
+        var subscription = new NotificationSubscription(chatFunction.parameters!.join(','));
+        final _notifier = MailNotifier();
+        _notifier.removeSubscription(subscription);
+        _addSystemMessage("Notification for ${subscription.keyword} has been deleted.");
+        break;
       case 'navigateTo':
         Navigator.pushNamed(context, chatFunction.parameters![0]);
         break;
       case 'performSearch':
         Navigator.pushNamed(context, '/search', arguments: chatFunction.parameters);
+        break;
+      case 'scanMail':
+        mailLoader.uploadMail(ImageSource.camera);
+        break;
+      case 'uploadMail':
+        mailLoader.uploadMail(ImageSource.gallery);
         break;
     }
   }
