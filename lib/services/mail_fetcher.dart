df33a0f552ac8d41d8bad8b41d51aed950457fec
sHuber <111936253+shiftClick@users.noreply.github.com>
2022-10-23 18:00:57 -0400

    Feature/mail piece edits (#640)
    * 2022-10-19 commit
    
    commit
    
    * Commit 2022-10-22
    
    * 2022-10-22 Commit3
    
    Commit 3
    
    * Commit 2022-10-22 -5
    
    * commit 2022-10-22 -6
    
    updated a little after seeing a flaw in links
    
    * Update mail_service_test.dart
    
    * 2022-10-23 -2
    
    commit2

--- lib/services/mail_fetcher.dart
+++ lib/services/mail_fetcher.dart
@@ -5,6 +5,9 @@ import '../models/MailPiece.dart';
 import 'package:summer2022/image_processing/google_cloud_vision_api.dart';
 import 'package:enough_mail/enough_mail.dart';
 import '../models/Digest.dart';
+import 'package:intl/intl.dart';
+import 'package:html/parser.dart';
+
 
 /// The `MailFetcher` class requests new mail from a mail server.
 class MailFetcher {
@@ -26,6 +29,7 @@ class MailFetcher {
       // Process each email
       for (final email in emails) {
         try {
+          debugPrint("Attempting to process email from " + email.decodeDate()!.toString());
           mailPieces.addAll(await _processEmail(email));
         } catch(e) {
           print("Unable to process individual email.");
@@ -53,9 +57,14 @@ class MailFetcher {
     // Get attachments with metadata and convert them to MailPieces
     final mailPieceAttachments = await _getAttachments(email);
     for (final attachment in mailPieceAttachments) {
-      mailPieces.add(
-          await _processMailImage(attachment, email.decodeDate()!, mailPieces.length));
+
+      MailPiece mp = await _processMailImage(email, attachment, email.decodeDate()!, mailPieces.length);
+
+      mailPieces.add(mp);
     }
+
+    debugPrint("Finished processing " + mailPieceAttachments.length.toString() +
+          " mailpieces for email on " + email.decodeDate()!.toString());
 
     return mailPieces;
   }
@@ -108,7 +117,7 @@ class MailFetcher {
   }
 
   /// Process an individual mail image, converting it into a MailPiece
-  Future<MailPiece> _processMailImage(
+  Future<MailPiece> _processMailImage(MimeMessage email,
       Attachment attachment, DateTime timestamp, int index) async {
     MailResponse ocrScanResult = await _getOcrScan(attachment.attachment);
 
@@ -120,7 +129,7 @@ class MailFetcher {
 
     final id = "${attachment.sender}-$timestamp-$index";
     var text = ocrScanResult.textAnnotations.first.text;
-    var mid = attachment.contentID; //todo: couldn't determine where MID might be at a first glance, this seemed fitting for now
+    var scanImgCID = attachment.contentID; //todo: couldn't determine where MID might be at a first glance, this seemed fitting for now
     //todo: save list of URLs found on the ocrScanResult (including text URLs, barcodes, and QR codes)
     //todo: save list of Emails found on the ocrScanResult
     //todo: save list of Phone Numbers found on the ocrScanResult
@@ -129,8 +138,101 @@ class MailFetcher {
     //todo: otherwise the date is probably fine since there is only one USPS ID email per day
     final emailId = timestamp.toString();
 
+
+    //this section of code finds the USPS mailpiece ID in the email associated with the
+    //image CID.  Useful in getting links per mailpiece.
+
+    String mailPieceId = "";
+    //based on test account, need to get 2nd level of parts to find image.  search in text/html part first
+    for (int x = 0; x < email.mimeData!.parts!.length; x++) {
+
+      if (email.mimeData!.parts!
+          .elementAt(x)
+          .contentType
+          ?.value
+          .toString()
+          .contains("multipart") ??
+          false) {
+        for (int y = 0;
+        y < email.mimeData!.parts!.elementAt(x).parts!.length;
+        y++) {
+          if (email.mimeData!.parts!
+              .elementAt(x)
+              .parts!
+              .elementAt(y)
+              .contentType
+              ?.value
+              .toString()
+              .contains("text/html") ??
+              false) {
+            //get the parts into an html document to make it searchable.
+            //need to decode Text into 'quoted-printable' type to see all the link text values
+            var doc = parse(email.mimeData!.parts!
+                .elementAt(x)
+                .parts!
+                .elementAt(y)
+                .decodeText(
+                ContentTypeHeader('text/html'), 'quoted-printable'));
+
+            //first step is to get all elements that are image, and have alt text 'scanned image of your mail piece'.
+            var scannedMailPieceItems = doc.querySelectorAll(
+                'img[alt*=\'Scanned image of your mail piece\']');
+
+            //scan through the mailpiece images to figure out which index matches the mailPiece Id.
+            //this will be used to find the corresponding reminder link.
+            int matchingIndex = -1;
+            for (int i = 0; i < scannedMailPieceItems.length; i++) {
+              if (scannedMailPieceItems[i]
+                  .attributes
+                  .toString()
+                  .contains(scanImgCID)) {
+                matchingIndex = i;
+                break;
+              }
+            }
+
+            //print debug error that the scanImgCID didn't find a match.
+            if (matchingIndex == -1) {
+              debugPrint("For mailPiece " + scanImgCID + " there was no associated ID.");
+              break;
+            }
+
+            //next, get a list of items that have the reminder link.  They all have the reminder link.
+            var reminderItems = doc.querySelectorAll(
+                'a[originalsrc*=\'informeddelivery.usps.com/box/pages/reminder\']');
+
+            //need a counter for times the reminder mailPiece with image was found
+            int reminderCount = 0;
+            //find a reminder with the image tag, this eliminates the duplicate tag with the "Set a Reminder" text
+            for (int i = 0; i < reminderItems.length; i++) {
+              if (reminderItems[i].innerHtml.toString().contains("img")) {
+                //we want to get the mailPieceID of the matching mailPiece.  Will help with getting other items
+                if (reminderCount == matchingIndex) {
+                  var regex = RegExp(
+                      r'mailpieceId=\d*\"'); //finds the string mailpieceId=digits to "
+                  var regexNum = RegExp(r'\d+'); //get numbers only
+
+                  var mpID1 = regex.firstMatch(
+                      reminderItems[i].outerHtml.toString());
+
+                  mailPieceId = regexNum.firstMatch(mpID1![0]!.toString())![0]!
+                      .toString();
+
+                  debugPrint("Date: " + DateFormat('yyyy/MM/dd').format(timestamp) + "; mailPieceCID: " + scanImgCID + "; has matching USPS-ID: " + mailPieceId);
+
+                  //break out of for after finding correct mailPiece
+                  break;
+                }
+                reminderCount++;
+              }
+            }
+          }
+        }
+      }
+    }
+
     return new MailPiece(
-        id, emailId, timestamp, attachment.sender, text!, mid);
+        id, emailId, timestamp, attachment.sender, text!, scanImgCID, mailPieceId);
   }
 
   /// Perform OCR scan once on the mail image to get the results for further processing
