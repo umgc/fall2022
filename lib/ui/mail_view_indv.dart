3011adb84a47ceea48ffb25fff55f7c320340815
cthorn2 <111912838+cthorn2@users.noreply.github.com>
2022-10-28 22:49:27 -0400

    Update to fix urls on mail actions panel.

--- lib/ui/mail_view_indv.dart
+++ lib/ui/mail_view_indv.dart
@@ -396,11 +396,14 @@ class MailPieceViewWidgetState extends State<MailPieceViewWidget> {
                                                       text = text.replaceAll(']', "");
                                                       text = text.replaceAll('[', "");
                                                       text = text.replaceAll(' ', "");
+                                                      //if(text.startsWith('www'))
+                                                      text = 'https://'+ text;
                                                     }
                                                     Uri uri = Uri.parse(text);
                                                     print(uri.toString());
+                                                    FirebaseAnalytics.instance.logEvent(name: 'Link_Navigated',parameters:{'itemId':text});
                                                     if (await launchUrl(uri)) {
-                                                      await launchUrl(uri);
+                                                      //await launchUrl(uri);
                                                     } else {
                                                       throw 'Could not launch ';
                                                     }
